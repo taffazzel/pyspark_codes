3db5a1e03efb554f7a1ef0ae5b3f25b2d87b6524
Taffazzel <tafaz_hossain@yahoo.com>
2018-01-12 22:12:36 -0500

    Cleaned and Validated

--- flight_analyis/data_clean_validate/clean_validate.py
+++ flight_analyis/data_clean_validate/clean_validate.py
@@ -7,17 +7,17 @@ import math
 def clean(body):
 
 	print"I am In Clean function"
-	cleanedRdd = body.map(lambda s:s.split(",")).filter(lambda s:len(s[0]) == 4).filter(lambda s:(int(s[1])) < 12).filter(lambda s: 1 <= (int(s[2])) <= 31 ).filter(lambda s: 1 <= (int(s[3])) <= 7).filter(lambda s: s[4].isdigit() and (int(s[4])) > 0).filter(lambda s: s[5].isdigit() and (int(s[5])) > 0).filter(lambda s: s[6].isdigit() and (int(s[6])) > 0).filter(lambda s: s[7].isdigit() and (int(s[7])) > 0).filter(lambda s:(len(s[8]))==2 and s[8]!="NA").filter(lambda s: (int(s[9])) > 0).filter(lambda s: s[10] != '0').filter(lambda s: s[11].isdigit and (int(s[11])) > 0).filter(lambda s: s[12].isdigit and (int(s[12])) > 0)
-	#for i in l.collect():
+	cleanedRdd = body.map(lambda s:s.split(",")).filter(lambda s: s[0].isdigit()).filter(lambda s:s[1].isdigit()).filter(lambda s: s[2].isdigit()).filter(lambda s: s[3].isdigit()).filter(lambda s: s[4].isdigit()).filter(lambda s: s[5].isdigit()).filter(lambda s: s[6].isdigit()).filter(lambda s: s[7].isdigit()).filter(lambda s:s[8]!="NA").filter(lambda s: s[9].isdigit()).filter(lambda s: s[10] != '0').filter(lambda s: s[11].isdigit()).filter(lambda s: s[12].isdigit())
+	#for i in cleanedRdd.collect():
 		#print i
 	#print l.count()
 	return cleanedRdd
 
 def validate(cleanedRdd):
 	print"I am validate function"
+	validatedRdd = cleanedRdd.filter(lambda s:len(s[0]) == 4).filter(lambda s:(int(s[1])) < 12).filter(lambda s: 1 <= (int(s[2])) <= 31 ).filter(lambda s: 1 <= (int(s[3])) <= 7).filter(lambda s: (int(s[4])) > 0).filter(lambda s: (int(s[5])) > 0).filter(lambda s: (int(s[6])) > 0).filter(lambda s: (int(s[7])) > 0).filter(lambda s:(len(s[8])) == 2).filter(lambda s: (int(s[9])) > 0).filter(lambda s: (int(s[11])) > 0).filter(lambda s:(int(s[12])) > 0)
+	return validatedRdd
 	
-
-
 
 
 
@@ -31,4 +31,6 @@ if __name__=="__main__":
 	header = lines.first()
 	body = lines.filter(lambda Row: Row!=header)
 	cleanedRdd = clean(body)
-	validate(cleanedRdd)
+	validatedRdd = validate(cleanedRdd)
+	for i in validatedRdd.collect():
+		print i
