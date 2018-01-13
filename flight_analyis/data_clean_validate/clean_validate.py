98589528fd96f5cd29b354cb89b4a9a0187065ed
Taffazzel <tafaz_hossain@yahoo.com>
2018-01-12 21:20:05 -0500

    Data cleansing code added. Though first 12 columns were applied for cleansing logic

--- flight_analyis/data_clean_validate/clean_validate.py
+++ flight_analyis/data_clean_validate/clean_validate.py
@@ -7,11 +7,15 @@ import math
 def clean(body):
 
 	print"I am In Clean function"
-	l = body.map(lambda s:s.split(",")).filter(lambda s:len(s[0]) == 4).filter(lambda s:(int(s[1])) < 12).filter(lambda s: 1 <= (int(s[2])) <= 31 ).filter(lambda s: 1 <= (int(s[3])) <= 7).filter(lambda s: s[4].isdigit() and (int(s[4])) > 0)
-	#l = body.map(lambda s: s.split(",")).map(lambda s:len(s[0]))
-	for i in l.collect():
-		print i
+	cleanedRdd = body.map(lambda s:s.split(",")).filter(lambda s:len(s[0]) == 4).filter(lambda s:(int(s[1])) < 12).filter(lambda s: 1 <= (int(s[2])) <= 31 ).filter(lambda s: 1 <= (int(s[3])) <= 7).filter(lambda s: s[4].isdigit() and (int(s[4])) > 0).filter(lambda s: s[5].isdigit() and (int(s[5])) > 0).filter(lambda s: s[6].isdigit() and (int(s[6])) > 0).filter(lambda s: s[7].isdigit() and (int(s[7])) > 0).filter(lambda s:(len(s[8]))==2 and s[8]!="NA").filter(lambda s: (int(s[9])) > 0).filter(lambda s: s[10] != '0').filter(lambda s: s[11].isdigit and (int(s[11])) > 0).filter(lambda s: s[12].isdigit and (int(s[12])) > 0)
+	#for i in l.collect():
+		#print i
 	#print l.count()
+	return cleanedRdd
+
+def validate(cleanedRdd):
+	print"I am validate function"
+	
 
 
 
@@ -26,4 +30,5 @@ if __name__=="__main__":
 	lines = sc.textFile("file:///root/flight_analysis/small")
 	header = lines.first()
 	body = lines.filter(lambda Row: Row!=header)
-	clean(body)
+	cleanedRdd = clean(body)
+	validate(cleanedRdd)
